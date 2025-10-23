51161d837fad5bc79c2e5fcf68359da70aadb799
Nicholas Nuti <54613418+nick-nuti@users.noreply.github.com>
2025-10-23 09:57:05 -0700

    Update required_packages.sv

--- required_packages.sv
+++ required_packages.sv
@@ -57,27 +57,3 @@ package axi_master_pkg;
     localparam int WR_DATA_W = DATA_W + STRB_W;
     localparam int WR_RESP_W = RESP_W + ID_W;
 endpackage
-
-package axi_pkg;
-    import axi_master_pkg::*;
-
-    typedef struct packed 
-    {
-        logic [ADDR_W-1:0] address;
-        logic [LEN_W-1:0]  awlen;
-        logic [ASIZE_W-1:0]  awsize;
-        logic [ID_W-1:0]  awid;
-    } wr_cmd_type;
-
-    typedef struct packed 
-    {
-        logic [DATA_W-1:0] wdata;
-        logic [STRB_W-1:0]  wstrb;
-    } wr_data_type;
-    
-    typedef struct packed
-    {
-        logic [RESP_W-1:0]     bresp;
-        logic [ID_W-1:0]       bid;
-    } wr_resp_type;
-endpackage
